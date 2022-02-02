4a046d373bcfc5bd6ad5f89a8c547daa0d4a9a24
dprashin <dprashin@gmail.com>
2022-02-02 12:20:55 +0100

    Add db context, seed extension method and configure Startup.cs file to use InMemory database

--- SuperSport.API/Startup.cs
+++ SuperSport.API/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SuperSport.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,8 @@ namespace SuperSport.API {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<ShopContext>(options =>
+            options.UseInMemoryDatabase("SuperShop"));
             services.AddControllers();
         }
 
