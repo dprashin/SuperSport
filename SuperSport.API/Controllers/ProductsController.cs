e045c4ca0de69360d2bbd77267978e395140455c
dprashin <dprashin@gmail.com>
2022-02-08 13:34:25 +0100

    API version controlling using header versioning

--- SuperSport.API/Controllers/ProductsController.cs
+++ SuperSport.API/Controllers/ProductsController.cs
@@ -10,7 +10,8 @@ using System.Threading.Tasks;
 
 namespace SuperSport.API.Controllers {
     [ApiVersion("1.0")]
-    [Route("v{v:apiVersion}/products")]
+    //[Route("v{v:apiVersion}/products")]
+    [Route("products")]
     [ApiController]
     public class ProductsV1_0Controller : ControllerBase {
 
@@ -151,7 +152,8 @@ namespace SuperSport.API.Controllers {
     }
 
     [ApiVersion("2.0")]
-    [Route("v{v:apiVersion}/products")]
+    //[Route("v{v:apiVersion}/products")]
+    [Route("products")]
     [ApiController]
     public class ProductsV2_0Controller : ControllerBase {
 
--- SuperSport.API/Startup.cs
+++ SuperSport.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +39,7 @@ namespace SuperSport.API {
                 options.ReportApiVersions = true;
                 options.DefaultApiVersion = new ApiVersion(1, 0);
                 options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = new HeaderApiVersionReader("X-API-Version");
             });
         }
 
